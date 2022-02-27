9b690ead917d5993f38f5dd85b7b503ee5a83b81
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-02-27 21:43:48 +0100

    Use a different derivation for the prebuilds path

--- lib/default.nix
+++ lib/default.nix
@@ -1,15 +1,22 @@
 system: { nixpkgs, ... } @ args: rec {
   inherit (builtins) toFile;
 
+  allPrebuilds = builtins.path {
+    path = ../prebuilt;
+    sha256 = "sha256-KCcpkzakViGAgcR8CECNuXghl1B3HREoTPfDLg2o9Ys=";
+  };
+
+  prebuilts = "${allPrebuilds}/${system}";
+
   baseDerivation = { script, ... } @args: builtins.derivation (args // {
     inherit system;
-    builder = ./. + "/../prebuilt/${system}/busybox";
+    builder = "${prebuilts}/busybox";
     args = [
       "sh"
       (toFile "${args.name}-builder.sh" (
         ''
           set -ex
-          export PATH=${./. + "/../prebuilt/${system}"}
+          export PATH=${prebuilts}
           eval "$script"
         ''
       ))
